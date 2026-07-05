49cf761e662c6496dd4eb19b70356a6b1cef8153
cixue <cixue.tsu@gmail.com>
2026-07-05 17:39:37 -0400

    Update feature registration module documentation

--- bandpass_classifier/features/registry.py
+++ bandpass_classifier/features/registry.py
@@ -1,9 +1,12 @@
-"""Feature registration module.
+"""Central feature registration module.
 
 This module imports all feature modules to ensure that decorators decorate classes
 and register functions correctly when the features package is loaded.
+
+To add a new feature set, implement the related feature construction process in a
+separate module and import it here.
 """
 
-from . import basic
-from .scan_statistics import features as scan_statistics_features
+from . import basic_features
+from .scan_statistics import features
 
